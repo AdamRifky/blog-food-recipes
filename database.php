8b8e1e3f78ce3ccbdb0d95b0e241142cee7d9f93
root <root@debian.w42t5pzidu0eje31dpx1pnfslh.px.internal.cloudapp.net>
2024-12-17 10:02:34 +0000

    database

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 
 $hostname = "localhost";
-$username = "root";
-$password = "";
+$username = "root2";
+$password = "1q2w3e4r";
 $database_name = "db_food_blog";
 
 $db = mysqli_connect($hostname, $username, $password, $database_name);
