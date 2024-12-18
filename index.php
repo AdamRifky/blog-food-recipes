2c894f14e959003bd05ab25bfae29984877c7795
Adam Rifky Saputra <146899383+AdamRifky@users.noreply.github.com>
2024-12-19 05:33:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>FOOD</title>
+  <title>FOOD ADAM</title>
   <link href="src/output.css" rel="stylesheet" />
 </head>
 
